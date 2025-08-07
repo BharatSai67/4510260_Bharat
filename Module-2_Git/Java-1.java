f7a5408069852cd2798d73ccb484f68b4386efb3
SATTI BHARAT SAI NAGA BABU <sattibharatsai67@gmail.com>
2025-08-07 11:23:49 +0530

    first commit

--- Module-2_Git/Java-1.java
+++ Module-2_Git/Java-1.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 class Test {
     public static void main(String[] args) {
+
         System.out.println("Hello World Java");
         System.out.println("Cutie Pie");
         System.out.println("Teddyluu");
